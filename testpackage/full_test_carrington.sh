d123724ae93038fb357eff65cfe78df3c8dd0ca9
Andrew PEDREA Young <git@pedrea.nz>
2026-07-15 16:02:39 +0300

    Fix testpackage
    Following fmihpc/vlasiator PR#1285 for mpi interface

--- testpackage/full_test_carrington.sh
+++ testpackage/full_test_carrington.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 12:45:00        # Run time (hh:mm:ss)
-#SBATCH --constraint="ukko"
+#SBATCH --constraint="amd"
 #SBATCH --nodes=1
 #SBATCH -c 4                 # CPU cores per task
 #SBATCH -n 8                  # number of tasks
@@ -15,8 +15,8 @@ module load OpenMPI/4.1.6-GCC-13.2.0
 module load PMIx/4.2.6-GCCcore-13.2.0
 module load PAPI/7.1.0-GCCcore-13.2.0
 module load Boost/1.83.0-GCC-13.2.0
-#export UCX_TLS=dc_mlx5
-export UCX_TLS=rc,sm
+export UCX_TLS=dc_mlx5
+#export UCX_TLS=rc,sm
 export UCX_NET_DEVICES=mlx5_0:1
 
 export OMPI_MCA_btl='^uct,ofi'
@@ -37,11 +37,10 @@ echo "Running $exec on $SLURM_NTASKS mpi tasks, with $t threads per task on $SLU
 # srun --mpi=pmix --cpu-bind=cores xthi
 
 # Run tests
-cd ~/ndccrg
+cd ~/ndccrg/$1
 export DCCRG_ARCH=carrington_gcc_openmpi
-#cd tests/refine
 export ASAN_OPTIONS=detect_leaks=0
-#make -j 16
-make test -j 16 -B
+make -j 16 # some executables should be check if they compiles
+make test -j 16
 #ASAN_OPTIONS=detect_leaks=0 make tests/get_cells/test1.tstN
 wait
