889aa4a573ceffc9e1fd372b39fc64bd445a7606
Andrew pedrea Young <git@pedrea.nz>
2026-09-23 12:39:01 +0300

    Use attribute instead of casting to void

--- dccrg_get_cell_datatype.hpp
+++ dccrg_get_cell_datatype.hpp
@@ -56,25 +56,17 @@ std::tuple<void*, int, MPI_Datatype> get_cell_mpi_datatype(
 
 // Compatibility macros
 #define UNUSED_GMD_ARGS \
-	const uint64_t cell_id, \
-	const int sender,       \
-	const int receiver,     \
-	const bool receiving,   \
-	const int neighborhood_id
-#define IGNORE_UNUSED_GMD_ARGS do {\
-	(void)cell_id;             \
-	(void)sender;              \
-	(void)receiver;            \
-	(void)receiving;           \
-	(void)neighborhood_id;     \
-	} while(0)
+	[[maybe_unused]] const uint64_t cell_id, \
+	[[maybe_unused]] const int sender,       \
+	[[maybe_unused]] const int receiver,     \
+	[[maybe_unused]] const bool receiving,   \
+	[[maybe_unused]] const int neighborhood_id
 #define DEFAULT_GMD_ARGS 0,0,0,0,0
 // struct types weapper
 template<typename T, std::size_t N, MPI_Datatype D>
 struct array_wrapper:std::array<T, N> {
 	std::tuple<void*, int, MPI_Datatype>
 	inline get_mpi_datatype(UNUSED_GMD_ARGS) {
-		IGNORE_UNUSED_GMD_ARGS;
 		auto cell = (std::array<T, N>)(*this);
 		return std::make_tuple((void*) cell.data(), cell.size(), D);
 	}
@@ -109,7 +101,6 @@ OS& operator<< (OS& os, const wrapper<T> x) {
 		T value;						    \
 		std::tuple<void*, int, MPI_Datatype>                        \
 		get_mpi_datatype(UNUSED_GMD_ARGS) {                         \
-			IGNORE_UNUSED_GMD_ARGS;				    \
 			return std::make_tuple((void*)this, 1, MPI_##type); \
 		}                                                           \
 		inline operator T() const { return this->value; } 	    \
